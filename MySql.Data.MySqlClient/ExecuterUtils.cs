f9091779b4939fcceb229ab68d57f7b0fafe2c90
28810 <28810@YEXIANGQIN>
2018-11-20 19:49:36 +0800

    v1.2.1

--- MySql.Data.MySqlClient/ExecuterUtils.cs
+++ MySql.Data.MySqlClient/ExecuterUtils.cs
@@ -47,7 +47,7 @@ namespace MySql.Data.MySqlClient {
 		}
 
 		private static DateTime dt1970 = new DateTime(1970, 1, 1);
-		private static Random rnd = new Random();
+		private static ThreadLocal<Random> rnd = new ThreadLocal<Random>();
 		private static readonly int __staticMachine = ((0x00ffffff & Environment.MachineName.GetHashCode()) +
 #if NETSTANDARD1_5 || NETSTANDARD1_6
 			1
@@ -56,7 +56,7 @@ namespace MySql.Data.MySqlClient {
 #endif
 			) & 0x00ffffff;
 		private static readonly int __staticPid = Process.GetCurrentProcess().Id;
-		private static int __staticIncrement = rnd.Next();
+		private static int __staticIncrement = rnd.Value.Next();
 		/// <summary>
 		/// 生成类似Mongodb的ObjectId有序、不重复Guid
 		/// </summary>
@@ -65,7 +65,7 @@ namespace MySql.Data.MySqlClient {
 			var now = DateTime.Now;
 			var uninxtime = (int) now.Subtract(dt1970).TotalSeconds;
 			int increment = Interlocked.Increment(ref __staticIncrement) & 0x00ffffff;
-			var rand = rnd.Next(0, int.MaxValue);
+			var rand = rnd.Value.Next(0, int.MaxValue);
 			var guid = $"{uninxtime.ToString("x8").PadLeft(8, '0')}{__staticMachine.ToString("x8").PadLeft(8, '0').Substring(2, 6)}{__staticPid.ToString("x8").PadLeft(8, '0').Substring(6, 2)}{increment.ToString("x8").PadLeft(8, '0')}{rand.ToString("x8").PadLeft(8, '0')}";
 			return Guid.Parse(guid);
 		}
